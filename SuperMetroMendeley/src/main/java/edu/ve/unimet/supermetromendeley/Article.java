71bc7c532b6c2577ba3d884f6645df8a38fd066f
Bianca Zullo <122377940+31065119@users.noreply.github.com>
2025-11-23 13:55:44 -0400

    Añadir y mostrar resumenes

---
+++ SuperMetroMendeley/src/main/java/edu/ve/unimet/supermetromendeley/Article.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ve.unimet.supermetromendeley;
+
+/**
+ *
+ * @author biancazullo
+ */
+public class Article {
+    public String title;
+    public List<String> authors;
+    public String body;
+    public List<String> keywords;
+    
+    public Article() {
+        this.authors = new List();
+        this.keywords = new List();
+        this.body = "";
+    }
+}
